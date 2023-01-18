6663624c09642c707912406e8e84df4fcf2158ef
Afnan <khayari@gcet.edu.om>
2023-01-19 00:52:33 +0400

    Online Shopping App

---
+++ app/src/main/java/com/OnlineShoppingApp/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.OnlineShoppingApp.shopping.Prevalent;
+
+import com.OnlineShoppingApp.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
